c4e390dba2f1747c774a5f257155e3994fc7481d
abdelgelil <abdelgelilahmed9@gmail.com>
2026-09-25 22:36:47 +0200

    fix: force express server to bind to 0.0.0.0 for Railway

--- client/Top Line/server.js
+++ client/Top Line/server.js
@@ -16,6 +16,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Frontend production server running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server started on 0.0.0.0:${PORT}`);
 });
